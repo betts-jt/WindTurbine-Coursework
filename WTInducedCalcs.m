1d562c2840114bf51387d35eb50e19c20df01597
betts-jt <01betjos@gmail.com>
2018-10-24 18:40:52 +0100

    Add the ability for the code to just solve for a if too many loops occur

--- WTInducedCalcs.m
+++ WTInducedCalcs.m
@@ -8,7 +8,7 @@ tol = 0.0001; % Setting the tollerence required between the input and output val
 loopCount = 0; % Setting up a counter to count the numebr of loops
 Error = 10; % Setting up an initial error value to enable the loop to start
 k = 0.1; % Relaxation factor used in loop up avoid an unstable loop situation
-loopCountMax = 100; %Define the maximum numebr of loops permetted to stop infinate looping
+loopCountMax = 100; %Define the maximum numebr of loops permetted whilst solving for both a and adash to stop infinate looping
 aNew = 0; % Setting the value a aNew to zero for the first loop
 adashNew = 0; % Setting the value a adashNew to zero for the first loop
 
@@ -40,14 +40,16 @@ while Error > tol
     aNew = 1/(((4*sin(phi)^2)/(sigma*Cn))+1); % Calcualting the new value of a
     adashNew = 1/(((4*(sin(phi)*cos(phi))/(sigma*Ct))-1)); % Calcualting the new value of adash
     
-    Error = abs(aNew-a)+abs(adashNew-adash); % Calculating the difference between the input and output values of a, adash
     
-    loopCount = loopCount+1; % Increase the loop counter by 1
-    
-    if loopCount == loopCountMax %End the while loop if the maximum desired loops has been reached
-        break
+    if loopCount < loopCountMax %If loop count is less than the desired maximum
+        Error = abs(aNew-a)+abs(adashNew-adash); % Calculating the difference between the input and output values of a, adash
+    elseif loopCount > loopCountMax % If loop count is above the desired maximum
+        Error = abs(aNew-a); % Calculating the difference between the input and output values of a, adash
+    elseif loopCount == 5*loopCountMax
+        error('It has not been possible to calculate a value of a. Please change the input parameters and try again'); % Stop trying to find a or adash is 5 times the maximum desired loops has been reached to stop the code running infinetly
     end
     
+    loopCount = loopCount+1; % Increase the loop counter by 1
 end
 
 %DISPLAY THE RESULTS OF THE AVOBE CALUCLATIONS
