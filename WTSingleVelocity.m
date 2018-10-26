148a78d71e61ceae707df57e51c90d3ffe06a02c
betts-jt <01betjos@gmail.com>
2018-10-26 20:11:30 +0100

    Update WTSingleVelocity.m

--- WTSingleVelocity.m
+++ WTSingleVelocity.m
@@ -7,6 +7,10 @@ function [MT, MN] = WTSingleVelocity(V0, Theta0, ThetaTwist, MeanChord, ChordGra
 a = 0; % Initial value of a dash used in Induced Calculatiuons
 adash = 0; %Initial value of a dash used in Induced Calculatiuons
 omega = 3.141593; % Initial tip velocity
+V0 = 20; % Initial velocity of wind perpendicular to turbine
+theta = 0.0733; % 
+Chord = 1; % Chord legnth of turbing blade
+B = 3; % Number of turbine blades
 
 % SETTING UP VALUES FOR FULL RADIUS CALCULATIONS
 N = 20; % The total numebr of sections acros the balde span to be analysed
@@ -14,8 +18,6 @@ span = TipRadius-RootRadius; %Total Legnth of Blade
 y(1:N) = [RootRadius:span/(N-1):TipRadius]; % Generate N points along the blade as values of span
 
 for i=1:N
-    [aNew, adashNew, phi, Cn, Ct] = WTInducedCalcs(a, adash, V0, omega, y(i), theta, Chord, B, dispInducedResults)
-    Vrel(i) = ((V0*(1-a))^2 + (omega*y(i)*(1+adash))^2)^0.5;
-    
+    [a_out(i), adash_out(i), phi(i), Cn(i), Ct(i), Vrel(i)] = WTInducedCalcs(a, adash, V0, omega, y(i), theta, Chord, B)
 end
 end
