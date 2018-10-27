a8b9e2effa0acd7315905764537d8a086f708627
betts-jt <01betjos@gmail.com>
2018-10-27 13:47:48 +0100

    add comments

--- WTSingleVelocity.m
+++ WTSingleVelocity.m
@@ -20,14 +20,16 @@ y(1:N-1) = [RootRadius+deltay/2:deltay:TipRadius-deltay/2]; % Generate N points 
 
 %RUN THE INDUCED VELOCITY CALCULATION FOR ALL POINTS ON SPAN
 for i=1:N-1
-    ThetaR(i) = Theta0+y(i)*ThetaTwist; % Calcualte the value to twist at the particular point on the blades span
-    Chord(i) = Chord0 + ((y(i)-(TipRadius/2))*ChordGrad); % 
-    [a_out(i), adash_out(i), phi(i), Cn(i), Ct(i), Vrel(i)] = WTInducedCalcs(a, adash, V0, omega, y(i), ThetaR(i), Chord(i), B);
+    ThetaR(i) = Theta0+y(i)*ThetaTwist; % Calcualte the value of twist at the particular point on the blades span
+    Chord(i) = Chord0 + ((y(i)-(TipRadius/2))*ChordGrad); % Calcualte the value of chord legnth at the particular point on the blades span
+    [a_out(i), adash_out(i), phi(i), Cn(i), Ct(i), Vrel(i)] = WTInducedCalcs(a, adash, V0, omega, y(i), ThetaR(i), Chord(i), B); % Run the induceed calculations at a specific poinot on the blade
 end
 
-Mt = (0.5*rho.*Vrel.^2.*Chord.*Ct)*deltay.*y;
-Mn = (0.5*rho.*Vrel.^2.*Chord.*Cn)*deltay.*y;
+Mt = (0.5*rho.*Vrel.^2.*Chord.*Ct)*deltay.*y; % Calcualte the moment due to torque at all points of the blade
+Mn = (0.5*rho.*Vrel.^2.*Chord.*Cn)*deltay.*y; % Calcualte the moment due to root bending at all points of the blade
 
-Mtott = sum(Mt);
-Mntot = sum(Mn);
+Mtott = sum(Mt); % Caluclate the total bending moment due to torque ofthe blade 
+Mntot = sum(Mn); % Calcualte the total root bending moment of the blade
+
+
 end
