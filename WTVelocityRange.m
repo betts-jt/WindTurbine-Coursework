8f68fa70da9d859130c809ae19f7ee67c1018d17
betts-jt <01betjos@gmail.com>
2018-10-27 15:18:37 +0100

    start velocity range code

--- WTVelocityRange.m
+++ WTVelocityRange.m
@@ -3,5 +3,17 @@ function [Diff] = WTVelocityRange(Parameters, A, k, omega, MeanChord, TipRadius,
 %entire velocity range. Combine this with the frequency information to get
 %the AEP.
 
+MinV0 = 5;
+MaxV0 = 25;
+A = 7;
+k = 1.8;
+
+% SET I:NITIAL VARIABLES FOR CALCULATION
+Interval = 1; %Setting the spacing between the min and max V0 to analyse
+
+V=[MinV0:Interval:MaxV0];
+
+for i=1:length(V)-1
+f(i) = exp(-(V(i)/A)^k)-exp(-(V(i+1)/A)^k);
 
 end
