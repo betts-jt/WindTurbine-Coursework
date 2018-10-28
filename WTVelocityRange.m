9fff464b5a56219cf50f9db550ee262217a92233
betts-jt <01betjos@gmail.com>
2018-10-28 21:37:49 +0000

    Change blade area calculation to anulus

--- WTVelocityRange.m
+++ WTVelocityRange.m
@@ -10,20 +10,19 @@ Interval = 1; %Setting the spacing between the min and max V0 to analyse
 Theta0 = Parameters(1);
 ThetaTwist = Parameters(2);
 ChordGrad = Parameters(3);
-BladeArea = pi()*TipRadius^2; % Calcualte the swept area of the blades
+BladeArea = pi()*(TipRadius^2-RootRadius); % Calcualte the swept area of the blades
 
 V=[MinV0:Interval:MaxV0];
 Vhalf = [MinV0+Interval/2:Interval:MaxV0-Interval/2];
 
 parfor i=1:length(V); % Run a parallal processing for loop
     [Mt, Mn,Power(i), y, a_out, adash_out, phi, Cn, Ct] = WTSingleVelocity(V(i), Theta0, ThetaTwist, MeanChord, ChordGrad, TipRadius, RootRadius, omega, B, BladeArea, rho);
-
 end
 
 for i=1:length(V)-1;
     f(i) = exp(-(V(i)/A)^k)-exp(-(V(i+1)/A)^k); % Calcualte the prabobability distribution for wind speeds
-    Power2(i) = (0.5*(Power(i)+Power(i+1))); % Calculate the power at heach value of Vhalf using the trapezium rule
-    AEPV(i) = 0.5*(Power(i)+Power(i+1))*f(i)*8760;  % Calcualte the anual energy  production using the trapezium rule
+    Power2(i) = (0.5*(Power(i)+Power(i+1))); % Calculate the power at each value of Vhalf using the trapezium rule
+    AEPV(i) = Power2(i)*f(i)*8760;  % Calcualte the anual energy  production using the trapezium rule
     BetzPower(i) = (16/27)*(0.5*rho*Vhalf(i)^3*BladeArea)*f(i)*8760; % Calcualte the ideal power generated at each windspeed using the Betz limit
 end
 
