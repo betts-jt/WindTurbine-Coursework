01c9c46b8d1fe6590273e873bd5c9d2a95a30372
betts-jt <01betjos@gmail.com>
2018-10-28 11:59:19 +0000

    add rho definition and pass to single velocity function

--- WTVelocityRange.m
+++ WTVelocityRange.m
@@ -4,18 +4,19 @@ function [Diff Vhalf, Power2, AEP, f] = WTVelocityRange(Parameters, A, k, omega,
 %the AEP.
 
 % SET INITIAL VARIABLES FOR CALCULATION
+rho = 1.225;
 Interval = 1; %Setting the spacing between the min and max V0 to analyse
 Theta0 = Parameters(1);
 ThetaTwist = Parameters(2);
 ChordGrad = Parameters(3);
-BladeArea = pi()*TipRadius^2 % Calcualte the swept area of the blades
+BladeArea = pi()*TipRadius^2; % Calcualte the swept area of the blades
 
 V=[MinV0:Interval:MaxV0];
 Vhalf = [MinV0+Interval/2:Interval:MaxV0-Interval/2];
 
 
 parfor i=1:length(V); % Run a parallal processing for loop
-[Mt, Mn,Power(i), Diff(i), y, a_out, adash_out, phi, Cn, Ct] = WTSingleVelocity(V(i), Theta0, ThetaTwist, MeanChord, ChordGrad, TipRadius, RootRadius, omega, B, BladeArea);
+[Mt, Mn,Power(i), Diff(i), y, a_out, adash_out, phi, Cn, Ct] = WTSingleVelocity(V(i), Theta0, ThetaTwist, MeanChord, ChordGrad, TipRadius, RootRadius, omega, B, BladeArea, rho);
 end
 
 for i=1:length(V)-1;
@@ -24,8 +25,8 @@ for i=1:length(V)-1;
 end
 
 parfor i=1:length(Vhalf); % Run a parallal processing for loop
-[Mt, Mn,Power2(i), Diff(i), y, a_out, adash_out, phi, Cn, Ct] = WTSingleVelocity(Vhalf(i), Theta0, ThetaTwist, MeanChord, ChordGrad, TipRadius, RootRadius, omega, B, BladeArea);
-BetzPower = (16/27)*(0.5*rho*vhalf^A)
+[Mt, Mn,Power2(i), Diff(i), y, a_out, adash_out, phi, Cn, Ct] = WTSingleVelocity(Vhalf(i), Theta0, ThetaTwist, MeanChord, ChordGrad, TipRadius, RootRadius, omega, B, BladeArea, rho);
+BetzPower = (16/27)*(0.5*rho*vhalf^A);
 end
 
 Vhalf = Vhalf';
