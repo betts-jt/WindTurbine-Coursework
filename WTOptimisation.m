5b7c8e351329fcaac6d3de16aba0af039d8fa27f
betts-jt <01betjos@gmail.com>
2018-10-28 16:20:58 +0000

    change where porblem variables are defines

--- WTOptimisation.m
+++ WTOptimisation.m
@@ -5,17 +5,6 @@ function [] = WTOptimisation(Initial, Lb, Ub)
     % initial guesses for Theta0, ThetaTwist and ChordGrad. Lb are the
     % lower bounds for the optimisation and Ub is the upper bounds.
     % An example input would be WTOptimisation([0.209 -0.00698 0],[0 -0.01 -0.5],[0.4 0 0.5])
-    
-% GENERATE A STRUCTURE OF VARIABLES
-var.A = 7; % Weibull Coefficient 
-var.k = 1.8; % Weibull Coefficient 
-var.omega = 3.1416; % Tip Speed
-var.MeanChord = 1; % Mean Chrod Radius
-var.TipRadius = 20; % Blade Tip radius
-var.RootRadius = 1; % Blade Root Radius
-var.B = 3; % Numebr of Blades
-var.MinV0 = 5; % Minimum wids speed for turbine to run (cut in speed)
-var.MaxV0 = 25 % Maximum speed of wind before turbine shuts down
 
 Theta0In = Initial(1);
 ThetaTwistIn = Initial(2);
@@ -33,9 +22,8 @@ opts = optimset('fminsearch');
 opts.Display = 'iter'; %What to display in command window
 opts.TolX = 0.0001; %Tolerance on the variation in the parameters
 opts.TolFun = 0.001; %Tolerance on the error
-opts.MaxIter = 100; %Max number of iterations
+opts.MaxIter = 5; %Max number of iterations
 
-[x] = fminsearchbnd(@DiffCost, [Initial], [Lb], [Ub], opts, var);
-
+[x, diff, exitflag] = fminsearchbnd(@DiffCost, [deg2rad(2.5) deg2rad(-1) 0.3], [deg2rad(2) deg2rad(-2) 0], [deg2rad(20) deg2rad(-0.1) 0.9], opts);
 
 end
