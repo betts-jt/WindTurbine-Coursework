5e625af2cb89adb0bb58db870545a8351909853e
betts-jt <01betjos@gmail.com>
2018-10-28 15:35:00 +0000

    Start of cost function

--- DiffCost.m
+++ DiffCost.m
@@ -1,7 +1,12 @@
-function [outputArg1,outputArg2] = DiffCost(inputArg1,inputArg2)
+function [Diff] = DiffCost(INPUT, var)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+Theta0 = INPUT(1);
+ThetaTwist = INPUT(2);
+ChordGrad = INPUT(3);
+
+[Diff, Vhalf, Power2, BetzPower AEPV, f, AEP] = WTVelocityRange([Theta0 ThetaTwist ChordGrad], var.A, var.k, var.omega, var.MeanChord, var.TipRadius, var.RootRadius, var.B, var.MinV0, var.MaxV0);
+
 end
 
