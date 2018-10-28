a5f13cb902c0dca2f330c17cdf2fdb7d934a0e36
betts-jt <01betjos@gmail.com>
2018-10-28 12:27:02 +0000

    edit to output correct power

--- WTVelocityRange.m
+++ WTVelocityRange.m
@@ -1,4 +1,4 @@
-function [Diff Vhalf, Power2, AEP, f] = WTVelocityRange(Parameters, A, k, omega, MeanChord, TipRadius, RootRadius, B, MinV0, MaxV0)
+function [Diff Vhalf, Power2, BetzPower AEP, f] = WTVelocityRange(Parameters, A, k, omega, MeanChord, TipRadius, RootRadius, B, MinV0, MaxV0)
 %3: ANNUAL ENERGY - loop WTSingleVelocity to find the moments across the
 %entire velocity range. Combine this with the frequency information to get
 %the AEP.
@@ -14,26 +14,27 @@ BladeArea = pi()*TipRadius^2; % Calcualte the swept area of the blades
 V=[MinV0:Interval:MaxV0];
 Vhalf = [MinV0+Interval/2:Interval:MaxV0-Interval/2];
 
-
+tic
 parfor i=1:length(V); % Run a parallal processing for loop
-[Mt, Mn,Power(i), Diff(i), y, a_out, adash_out, phi, Cn, Ct] = WTSingleVelocity(V(i), Theta0, ThetaTwist, MeanChord, ChordGrad, TipRadius, RootRadius, omega, B, BladeArea, rho);
+    [Mt, Mn,Power(i), Diff(i), y, a_out, adash_out, phi, Cn, Ct] = WTSingleVelocity(V(i), Theta0, ThetaTwist, MeanChord, ChordGrad, TipRadius, RootRadius, omega, B, BladeArea, rho);
+
 end
+toc
 
 for i=1:length(V)-1;
     f(i) = exp(-(V(i)/A)^k)-exp(-(V(i+1)/A)^k);
+    Power2(i) = (0.5*(Power(i)+Power(i+1)));
     AEP(i) = sum(0.5*(Power(i))+Power(i+1))*f(i)*8760;
-end
-
-parfor i=1:length(Vhalf); % Run a parallal processing for loop
-[Mt, Mn,Power2(i), Diff(i), y, a_out, adash_out, phi, Cn, Ct] = WTSingleVelocity(Vhalf(i), Theta0, ThetaTwist, MeanChord, ChordGrad, TipRadius, RootRadius, omega, B, BladeArea, rho);
-BetzPower = (16/27)*(0.5*rho*vhalf^A);
+    BetzPower(i) = (16/27)*(0.5*rho*Vhalf(i)^3*BladeArea);
 end
 
 Vhalf = Vhalf';
 AEP = AEP';
 f = f';
 Power2 = Power2';
-T = table(Vhalf, Power2, AEP, f)
+BetzPower = BetzPower';
+
+T = table(Vhalf, Power2, BetzPower, AEP, f)
 
 end
 
