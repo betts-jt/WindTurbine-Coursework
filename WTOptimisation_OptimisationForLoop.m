f365e20783fbc2ea6f2c873f6881536f27996577
betts-jt <01betjos@gmail.com>
2018-11-10 20:43:55 +0000

    edit function to run the for loop correctly

--- WTOptimisation_OptimisationForLoop.m
+++ WTOptimisation_OptimisationForLoop.m
@@ -1,20 +1,26 @@
-function [x] = WTOptimisation()
+function [x_Final, Diff_final] = WTOptimisation_OptimisationForLoop()
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
 
 addpath('Lib'); %Add Lib folder to path to enable functions within that folder to be used in this function
-%4: OPTIMISATION - use fminsearchbnd to optimise theta0, thetatw, and cgrad for
-%maximum AEP.
-    % Initial, Lb & Ub are both arrays of 3 numbers with Initial being the
-    % initial guesses for Theta0, ThetaTwist and ChordGrad. Lb are the
-    % lower bounds for the optimisation and Ub is the upper bounds.
-    % An example input would be WTOptimisation([0.209 -0.00698 0],[0 -0.01 -0.5],[0.4 0 0.5])
 
 opts = optimset('fminsearch');
 opts.Display = 'iter'; %What to display in command window
 opts.TolX = 0.0001; %Tolerance on the variation in the parameters
 opts.TolFun = 0.001; %Tolerance on the error
-opts.MaxIter = 100; %Max number of iterations
+opts.MaxIter = 10; %Max number of iterations
 
-[x, diff, exitflag] = fminsearchbnd(@DiffCost, [deg2rad(2.5) deg2rad(-1) 0], [deg2rad(2) deg2rad(-2) -0.1], [deg2rad(20) deg2rad(-0.1) 0.1], opts);
+Theta0 = [2:((20-2)/9):20];
+ThetaTW = [-2:(-0.1--2)/9:-0.1];
+Chordgrad = [-0.1:(0.1--0.1)/9:0.1];
 
-x=[rad2deg(x(1)) rad2deg(x(2)) x(3)];
+for i = 1:10
+[x(i,:), diff(i), exitflag] = fminsearchbnd(@DiffCost, [deg2rad(2.5) deg2rad(-1) 0], [deg2rad(2) deg2rad(-2) -0.1], [deg2rad(20) deg2rad(-0.1) 0.1], opts);
+end
+
+[Diff_final,Diff_pos]=min(diff);
+x_Final = x(Diff_pos,:);
+
+
+
 end
