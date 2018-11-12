c1389a2aba33b08870729e5a0a9315e9e262d0b7
betts-jt <01betjos@gmail.com>
2018-11-12 20:49:58 +0000

    make function to run the values for a blade once

---
+++ SingleRun.m
@@ -0,0 +1,22 @@
+function [outputArg1,outputArg2] = SingleRun(Theta0 ThetaTwist ChordGrad)
+% This function runs the velocity calculatiuons for a single blade to give
+% details of how it performed and details relating to its possible AEP and
+% the deflection of the blade when in  use
+
+
+% GENERATE A STRUCTURE OF VARIABLES
+variables.A = 7; % Weibull Coefficient 
+variables.k = 1.8; % Weibull Coefficient 
+variables.omega = 3.1416; % Tip Speed
+variables.MeanChord = 1; % Mean Chrod Radius
+variables.TipRadius = 20; % Blade Tip radius
+variables.RootRadius = 1; % Blade Root Radius
+variables.B = 3; % Numebr of Blades
+variables.MinV0 = 5; % Minimum wids speed for turbine to run (cut in speed)
+variables.MaxV0 = 25; % Maximum speed of wind before turbine shuts down
+
+%RUN THE VELOCITY RANGE FUNCTION
+[Diff, Vhalf, Power2, BetzPower, AEPV, f, AEP] = WTVelocityRange([Theta0 ThetaTwist ChordGrad], variables.A, variables.k, variables.omega, variables.MeanChord, variables.TipRadius, variables.RootRadius, variables.B, variables.MinV0, variables.MaxV0);
+
+end
+
