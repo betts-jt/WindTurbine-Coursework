190968645cbcfb7001102640507d9929ec9830c4
betts-jt <01betjos@gmail.com>
2018-11-13 13:58:32 +0000

    Change how the number of loops is set

--- WTOptimisation_OptimisationForLoop.m
+++ WTOptimisation_OptimisationForLoop.m
@@ -6,17 +6,19 @@ function [x_Final, Diff_final] = WTOptimisation_OptimisationForLoop()
 
 addpath('Lib'); %Add Lib folder to path to enable functions within that folder to be used in this function
 
+NumStartingPoints = 20; % Total number of starting poinmts for the optimiser
+
 opts = optimset('fminsearch');
 opts.Display = 'iter'; %What to display in command window
-opts.TolX = 0.0001; %Tolerance on the variation in the parameters
+opts.TolX = 0.01; %Tolerance on the variation in the parameters
 opts.TolFun = 0.01; %Tolerance on the error
 opts.MaxIter = 100; %Max number of iterations
 
-Theta0 = [2:((20-2)/9):20];
-ThetaTW = [-2:(-0.1--2)/9:-0.1];
-Chordgrad = [-0.1:(0.1--0.1)/9:0.1];
+Theta0 = [2:((20-2)/(NumStartingPoints-1)):20];
+ThetaTW = [-2:(-0.1--2)/(NumStartingPoints-1):-0.1];
+Chordgrad = [-0.1:(0.1--0.1)/(NumStartingPoints-1):0.1];
 
-for i = 1:10
+for i = 1:NumStartingPoints
 [x(i,:), diff(i), exitflag] = fminsearchbnd(@DiffCost, [deg2rad(Theta0(i)) deg2rad(ThetaTW(i)) Chordgrad(i)], [deg2rad(2) deg2rad(-2) -0.1], [deg2rad(20) deg2rad(-0.1) 0.1], opts);
 end
 
